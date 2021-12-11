8ecb65171dd6f8c0c34e4712628064916de8c55e
Christopher Keith <chris.keith@gmail.com>
2021-12-11 11:31:54 -0800

    Better formatting for example email.

--- airtable/app1.js
+++ airtable/app1.js
@@ -2,29 +2,32 @@ class App1 {
     buildBody(rec) {
         return `You are receiving this email because you have applied for eviction/rental help from Neighborhood Preservation, Inc.
 
-You have a court eviction hearing on ${rec.getCellValue('Next Court Date')} 
-at building: ${rec.getCellValue('Next Court Date Location')}, 
-in room" ${rec.getCellValue('Next Court Date Room')}. Your case number is ${rec.getCellValue('Eviction Case Number')}.
+You have a court eviction hearing on ${rec.getCellValue('Next Court Date')} \
+in building ${rec.getCellValue('Next Court Date Location')}, \
+room ${rec.getCellValue('Next Court Date Room')}. Your case number is ${rec.getCellValue('Eviction Case Number')}.
 
-You may have received U.S. Postal mail from the court containing a court date and location. That information may be incorrect. 
-To verify your court date, you can call the Court Clerk at (901) 222-3500. <b>What should the phone number be?</b>. 
-You can also view your case information at 
+You may have received U.S. Postal mail from the court containing a court date and location. That information may be incorrect.
+
+To verify your court date, you can call the Court Clerk at (901) 222-3500. ***What should the phone number be?***. \
+You can also view your case information at \
 https://gscivildata.shelbycountytn.gov/pls/gnweb/ck_public_qry_doct.cp_dktrpt_frames?case_id=${rec.getCellValue('Eviction Case Number')}.
 
-Please plan to arrive at court <i>at least</i> 15 minutes early to find parking and get to your courtroom. <b>Any other tips here?</b>
+Please plan to arrive at court *at least* 15 minutes early to find parking and get to your courtroom. ***Any other tips here?***
 
 This message was sent by an automated system from https://npimemphis.org. Please do not reply. Thank you.`
     }
     buildEmail(rec) {
-        return `Hello ${rec.getCellValue('AppFirstName')} ${rec.getCellValue('AppLastName')}, \
-\
+        return `Hello ${rec.getCellValue('AppFirstName')} ${rec.getCellValue('AppLastName')},
+
 ${this.buildBody(rec)}`
     }
     showEmail(rec) {
-        console.log('To: ' + rec.getCellValue('ApplicantEmail'));
-        console.log('From: do-not-reply@npimemphis.org');
-        console.log('Subject: ' + 'Your court eviction hearing');
-        console.log('Body: ' + this.buildEmail(rec));
+        let msg = `To: ${rec.getCellValue('ApplicantEmail')}
+From: do-not-reply@npimemphis.org
+Subject: Your court eviction hearing
+
+${this.buildEmail(rec)}`
+        console.log(msg)
     }
     runIt(query) {
         let alreadyMarkedRecords = query.records.filter(rec => {
