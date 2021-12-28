77ec702473ee92df3c6f6a61b739bfd30d4b1e7b
Christopher Keith <chris.keith@gmail.com>
2021-12-27 16:32:09 -0800

    Update conditions. Remove unused code.

--- airtable/app1.js
+++ airtable/app1.js
@@ -1,34 +1,4 @@
 class App1 {
-    buildBody(rec) {
-        return `You are receiving this email because you have applied for eviction/rental help from Neighborhood Preservation, Inc.
-
-You have a court eviction hearing on ${rec.getCellValue('Next Court Date')} \
-in building ${rec.getCellValue('Next Court Date Location')}, \
-room ${rec.getCellValue('Next Court Date Room')}. Your case number is ${rec.getCellValue('Eviction Case Number')}.
-
-You may have received U.S. Postal mail from the court containing a court date and location. That information may be incorrect.
-
-To verify your court date, you can call the Court Clerk at (901) 222-3500. ***What should the phone number be?***. \
-You can also view your case information at \
-https://gscivildata.shelbycountytn.gov/pls/gnweb/ck_public_qry_doct.cp_dktrpt_frames?case_id=${rec.getCellValue('Eviction Case Number')}.
-
-Please plan to arrive at court *at least* 15 minutes early to find parking and get to your courtroom. ***Any other tips here?***
-
-This message was sent by an automated system from https://npimemphis.org. Please do not reply. Thank you.`
-    }
-    buildEmail(rec) {
-        return `Hello ${rec.getCellValue('AppFirstName')} ${rec.getCellValue('AppLastName')},
-
-${this.buildBody(rec)}`
-    }
-    showEmail(rec) {
-        let msg = `To: ${rec.getCellValue('ApplicantEmail')}
-From: do-not-reply@npimemphis.org
-Subject: Your court eviction hearing
-
-${this.buildEmail(rec)}`
-        console.log(msg)
-    }
     runIt(query) {
         let alreadyMarkedRecords = query.records.filter(rec => {
             return (rec.getCellValue('Email For Automation'))
@@ -43,13 +13,18 @@ ${this.buildEmail(rec)}`
             if (status && status[0] && status[0]['name']) {
                 status_val = status[0]['name']
             }
+            let judge = rec.getCellValue('Next Court Date Judge')
+            if (!judge || judge === 'unassigned') {
+                judge = ''
+            }
             return (status_val.includes('Approved: Sent for legal') &&
                     caseNumber !== 'Not Found' &&
                     caseNumber !== '' &&
                    !rec.getCellValue('Tenant Case Email Sent') &&
-                    rec.getCellValue('ApplicantEmail') &&                   
-                    rec.getCellValue('Next Court Date') &&            
+                    rec.getCellValue('ApplicantEmail') &&
+                    rec.getCellValue('Next Court Date') &&
                     rec.getCellValue('Next Court Date Location') &&
+                    judge !== '' &&
                     rec.getCellValue('Next Court Date Room') 
                    )
         })
