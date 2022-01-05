ce1a49d251f36726d93d3e433c2a525e727980f6
Christopher Keith <chris.keith@gmail.com>
2022-01-05 09:36:00 -0800

    Use fetch. Remove unused email code. Need to debug network error.

--- airtable/notify_client_of_court_date.js
+++ airtable/notify_client_of_court_date.js
@@ -1,21 +1,4 @@
 class TextSender {
-    httpPost(url, body, rawPhone) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url);
-        xhr.setRequestHeader("Accept", "application/xml");
-        xhr.setRequestHeader("Content-Type", "application/xml");
-        xhr.onreadystatechange = function () {
-            console.log('xhr.responseText: ' + xhr.responseText + ' for ' + rawPhone)
-            if (xhr.readyState === 4) {
-                if (!xhr.responseText.includes('<ok/>')) {
-                    console.log('Error: ' + xhr.responseText + ' for ' + rawPhone)
-                }
-            } else {
-                console.log('xhr.readyState: ' + xhr.readyState + ' for ' + rawPhone)
-            }
-        };
-        xhr.send(body);
-    }
     async sendOne(rec) {
         let rawPhone = rec.getCellValue('ApplicantMobile')
         let ph = rawPhone.replace(/\D/g, '')
@@ -26,7 +9,7 @@ class TextSender {
             let theDate = rec.getCellValue('Next Court Date')
             let theLoc = rec.getCellValue('Next Court Date Location')
             let theRoom = rec.getCellValue('Next Court Date Room') 
-            let html = `<?xml version="1.0"?>
+            let xml = `<?xml version="1.0"?>
 <sms>
     <auth_key></auth_key>
     <command>send_message</command>
@@ -46,36 +29,24 @@ This message was sent by an automated system from https://npimemphis.org. Please
         <contact_number>+1${ph}</contact_number>
     </contacts>
 </sms>`
-            this.httpPost('https://www.txt180.com/members/api.php', html, rawPhone)
-            let now = new Date()
-            table.updateRecordAsync(rec, {'Tenant Case SMS Sent' : now})
+            let response = await fetch('https://www.txt180.com/members/api.php', {method: 'POST', body: xml});
+            let ret = await response.text();
+            if (!ret.includes('<ok/>')) {
+                console.log('Error: ' + ret + ' for ' + rawPhone)
+            } else {
+                let now = new Date()
+                table.updateRecordAsync(rec, {'Tenant Case SMS Sent' : now})
+            }
         }
     }
 }
 
 class App1 {
-    async setItUp(query) {
-        let alreadyMarkedRecords = query.records.filter(rec => {
-            return (rec.getCellValue('Email For Automation'))
-        })
-        let c1 = 0
-        for (let rec of alreadyMarkedRecords) {
-            table.updateRecordAsync(rec, {'Email For Automation' : ''})
-            c1++
-        }
-        console.log(c1 + ' Email For Automation value(s) cleared')
-    }
-    async triggerEmail(rec) {
-        let now = new Date()
-        let email = rec.getCellValue('ApplicantEmail')
-        table.updateRecordAsync(rec, {'Email For Automation' : email})
-        table.updateRecordAsync(rec, {'Tenant Case Email Triggered' : now})
-    }
     async sendSMS(rec) {
         let ts = new TextSender()
         await ts.sendOne(rec)
     }
-    async runIt(query, emailOrPhoneColumn) {
+    async runIt(query) {
         let filteredRecords = query.records.filter(rec => {
             let caseNumber = rec.getCellValue('Eviction Case Number')
             let status = rec.getCellValue('Case Status (In Neighborly)')
@@ -88,13 +59,8 @@ class App1 {
                 judge = ''
             }
             let sentStatusColName;
-            if (emailOrPhoneColumn === 'ApplicantEmail') {
-                sentStatusColName = 'Tenant Case Email Triggered'
-            } else {
-                sentStatusColName = 'Tenant Case SMS Sent'
-            }
-            let canSend = !rec.getCellValue(sentStatusColName) &&
-                       rec.getCellValue(emailOrPhoneColumn)
+            sentStatusColName = 'Tenant Case SMS Sent'
+            let canSend = !rec.getCellValue(sentStatusColName)
             return (status_val.includes('Approved: Sent for legal') &&
                     caseNumber !== 'Not Found' &&
                     caseNumber !== '' &&
@@ -107,26 +73,13 @@ class App1 {
         })
         let c = 0
         for (let rec of filteredRecords) {
-            if (emailOrPhoneColumn === 'ApplicantEmail') {
-                await this.triggerEmail(rec)
-            } else {
-                await this.sendSMS(rec)
-            }
+            await this.sendSMS(rec)
             c++
         }
-        if (emailOrPhoneColumn === 'ApplicantEmail') {
-            console.log('Marked ' + c + ' applicant(s) for automated email sending.')
-        } else {
-            console.log('Sent text messages to ' + c + ' applicant(s).')
-        }
-    }
-    async triggerEmails(query) {
-        await app.setItUp(query)
-        await app.runIt(query, 'ApplicantEmail')
+        console.log('Sent text messages to ' + c + ' applicant(s).')
     }
 }
 let table = base.getTable('All Applicants');
 let query = await table.selectRecordsAsync({fields: table.fields});
 let app = new App1()
-await app.triggerEmails(query)
-// await app.runIt(query, 'ApplicantMobile')
+await app.runIt(query)
