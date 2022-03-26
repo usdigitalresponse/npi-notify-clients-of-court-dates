480fdbf71ffaa2e7181a45e0e6913dde635f177e
Christopher Keith <chris.keith@gmail.com>
2022-03-26 10:22:48 -0700

    Look for settlements.

--- AddressScraper.py
+++ AddressScraper.py
@@ -19,20 +19,22 @@ class AddressScraper:
         numCases = len(cases)
         for c in cases:
             hashByCaseNumber[c['Eviction Case Number']] = self.caseScraper.get(c['Eviction Case Number'])
-        self.log(theLastInitial + ',numCases: ' + str(numCases) + ',len(hashByCaseNumber): ' + str(len(hashByCaseNumber)))
+        # self.log(theLastInitial + ',numCases: ' + str(numCases) + ',len(hashByCaseNumber): ' + str(len(hashByCaseNumber)))
         return [numCases, round(end - start)]
-    def getByAlpha(self, startLetter, endLetter, hashByCaseNumber):
-        totalstart = time.time()
-        for i in range(ord(startLetter), ord(endLetter) + 1):
-            letter = chr(i)
-            self.sendQuery(self.theDate, letter, hashByCaseNumber)
+    def logProgress(self, totalStart, startLetter, endLetter, numCases):
         totalEnd = time.time()
-        totalSeconds = int(round(totalEnd - totalstart))
+        totalSeconds = int(round(totalEnd - totalStart))
         minutes = round(totalSeconds / 60)
         seconds = round(totalSeconds % 60)
         elapsedTime = '{0:0>2}:{1:0>2}'.format(minutes, seconds)
         self.log(startLetter + "-" + endLetter + "," + self.theDate + "," +
-                str(len(hashByCaseNumber)) + "," + elapsedTime)
+                str(numCases) + "," + elapsedTime)
+    def getByAlpha(self, startLetter, endLetter, hashByCaseNumber):
+        totalStart = time.time()
+        for i in range(ord(startLetter), ord(endLetter) + 1):
+            letter = chr(i)
+            self.sendQuery(self.theDate, letter, hashByCaseNumber)
+        # self.logProgress(totalStart, startLetter, endLetter, len(hashByCaseNumber))
     def getByWildCard(self):
         wildcard_cases = {}
         retVals = self.sendQuery(self.theDate, "*", wildcard_cases)
@@ -47,14 +49,15 @@ class AddressScraper:
             else:
                 msg = '___'
             print(msg + "," + key + "," + wildcard_cases[key])
-    def dumpAddresses(self, case):
-        print(case['description']['filing_date'])
+    def findSettlements(self, case):
+        for entry in case['docket_entries']:
+            if entry['description'] == 'POSSESSION $___& COST FED':
+                print(case['description']['case_num'] + "|" + entry)
     def run(self):
         a_z_cases = {}
-        self.getByAlpha("b", "b", a_z_cases)
+        self.getByAlpha("a", "z", a_z_cases)
         for case_num in list(a_z_cases):
-            self.dumpAddresses(a_z_cases[case_num])
-            break
+            self.findSettlements(a_z_cases[case_num])
 
 if __name__ == "__main__":
     AddressScraper().run()
